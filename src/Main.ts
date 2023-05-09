009ce00349d8c12e9b863e305f0a039ea6209a37
McBen <viertelvor12@gmx.net>
2023-05-09 14:01:42 +0200

    add layercount similar toobar button
    # Conflicts:
    #	src/Main.ts
    #	src/ui/debugDialog.tsx

--- src/Main.ts
+++ src/Main.ts
@@ -2,8 +2,10 @@ import * as Plugin from "iitcpluginkit";
 import * as S2 from "./lib/s2";
 import { FieldLogger } from "./fieldLogger";
 import { MindunitsDB, S2MUDetailLevel, S2MULevel } from "./mindunitsDB";
+import myicon from "./ui/images/icon.svg";
 
-const TOOLTIP_DELAY = 1000;
+
+const TOOLTIP_DELAY = 100;
 
 
 class LogFields implements Plugin.Class {
@@ -15,10 +17,14 @@ class LogFields implements Plugin.Class {
     private mustrings: Map<string, L.Marker> = new Map();
     private s2Cells: L.LayerGroup<any> | undefined;
 
+    private trackingActive: boolean;
     private mouseDelayTimer: number | undefined;
-    private popupActive: boolean;
+    private tooltip: JQuery | undefined;
+
 
     async init() {
+
+        require("./ui/styles.pcss");
 
         this.fieldLog = new FieldLogger();
         this.fieldLog.init();
@@ -27,30 +33,25 @@ class LogFields implements Plugin.Class {
 
         window.addHook("fieldAdded", this.onFieldAdd);
         window.addHook("fieldRemoved", this.onFieldRemoved);
-        window.map.on("mousemove", this.onMouseMove);
 
         this.layer = new L.LayerGroup();
         window.addLayerGroup("MUs", this.layer, true);
 
-        this.setupCss();
-
         this.muDB = new MindunitsDB();
         this.muDB.train(this.fieldLog);
-    }
 
 
-    private setupCss(): void {
-        $("<style>")
-            .prop("type", "text/css")
-            .html(".plugin-logfields-numbers {\
-                font-size: 12px;\
-                color: #121230;\
-                font-family: monospace;\
-                text-align: center;\
-                pointer-events: none; \
-              }")
-            .appendTo("head");
+        const toolbarGroup = $("<div>", { class: "leaflet-bar leaflet-control plugin-logfields-icon", id: "logfieldbutton"})
+            .append(
+                $("<a>", { class: "leaflet-bar-part" })
+                    .css("background-image", myicon)
+                    .on("click", (() => this.toggleTracking())
+                ));
+
+        const parent = $(".leaflet-top.leaflet-left", window.map.getContainer()).first();
+        parent.append(toolbarGroup);
     }
+
 
 
     onFieldAdd = async (fieldEvent: EventFieldAdded): Promise<void> => {
@@ -99,21 +100,10 @@ class LogFields implements Plugin.Class {
     }
 
 
-    onMouseMove = (ev: L.LeafletMouseEvent) => {
-        window.clearTimeout(this.mouseDelayTimer);
-        this.mouseDelayTimer = window.setTimeout(() => this.checkForTooltip(ev), TOOLTIP_DELAY);
-
-        if (this.popupActive) {
-            window.map.closePopup();
-            this.popupActive = false;
-            this.clearS2Cells();
-        }
-    }
-
-
     checkForTooltip(ev: L.LeafletMouseEvent): void {
         const point = ev.layerPoint;
         const fields = [];
+        const drawTools: L.Polygon[] = [];
 
         for (var guid in window.fields) {
             const field = window.fields[guid];
@@ -127,19 +117,22 @@ class LogFields implements Plugin.Class {
 
         const dt = window.plugin.drawTools && window.plugin.drawTools.drawnItems;
         if (dt) {
-            window.plugin.drawTools.drawnItems.eachLayer((layer: L.ILayer) => {
+            dt.eachLayer((layer: L.ILayer) => {
                 if (layer instanceof L.GeodesicPolygon || layer instanceof L.Polygon) {
-                    const latlngs = layer.getLatLngs();
+                    const positions: L.Point[][] = (<any>layer)._rings;
+
+                    if (positions && this.pnpoly(positions[0], point)) {
+                        drawTools.push(layer);
+                    }
 
                 }
             })
         }
 
-        if (fields.length > 0) {
-            this.showTooltip(ev.latlng, fields);
+        if (fields.length > 0 || drawTools.length > 0) {
+            this.createTooltip(ev.latlng, fields, drawTools);
         } else {
-            window.map.closePopup();
-            this.clearS2Cells();
+            this.hideTooltip();
         }
     }
 
@@ -153,32 +146,93 @@ class LogFields implements Plugin.Class {
         return !!inside;
     }
 
-    async showTooltip(pos: L.LatLng, fields: IITC.Field[]): Promise<void> {
 
+    async createTooltip(pos: L.LatLng, fields: IITC.Field[], drawTools: L.Polygon[]): Promise<void> {
+
+        // real Fields
         fields = fields.sort((a, b) => this.triangleArea(b.getLatLngs()) - this.triangleArea(a.getLatLngs()));
-
-
         let total = 0;
-        const text: string[] = await Promise.all(fields.map(async f => {
-
-            const calcMU = this.muDB.calcMU(f.getLatLngs());
-            const calcMUStr = calcMU.missing ? ` >${calcMU.mindunits}` : calcMU.mindunits;
-
-            const mindunits = await this.fieldLog.getFieldMUS(f);
-            if (mindunits) {
-                total += mindunits;
-                return `${mindunits} Mus (~${calcMUStr})`;
-
-            } else {
-                total += calcMU.mindunits;
-                return `~${calcMUStr} Mus`;
-            }
+        let text: string[] = await Promise.all(fields.map(async f => {
+            const { text, mindunits } = await this.getFieldMUText(f);
+            total += mindunits;
+            return text;
         }))
 
-        if (total > 0) text.push(`<hr><br>Total: ${total}`);
+        // drawtools
+        let dttotal = 0;
+        const text2: string[] = await Promise.all(drawTools.map(async p => {
+            const { text, mindunits } = await this.getDTPolygonText(p);
+            dttotal += mindunits;
+            return text;
+        }))
 
-        window.map.openPopup(text.join("<br>"), pos);
-        this.popupActive = true;
+        if (text2.length > 0) {
+            text = [...text, "DrawTools:", ...text2]
+        }
+
+        // Summary
+        if (total > 0) {
+            text.push(`<hr>${fields.length} Fields = ${window.digits(total)} Mu`);
+        }
+        if (dttotal > 0) {
+            const sep = total > 0 ? "" : "<hr>";
+            text.push(`${sep}DrawTools = ${window.digits(dttotal)} Mu`);
+        }
+        if (total > 0 && dttotal > 0) {
+            text.push(`Total ${window.digits(total+dttotal)} Mu`);
+        }
+
+        this.showTooltip(pos, text.join("<br>"));
+    }
+
+
+    private async getFieldMUText(field: IITC.Field): Promise<{ text: string, mindunits: number }> {
+
+
+        const calcMU = this.muDB.calcMU(field.getLatLngs());
+        const calcMUStr = (calcMU.missing ? " >" : "") + window.digits(calcMU.mindunits);
+
+        const mindunits = await this.fieldLog.getFieldMUS(field);
+        if (mindunits) {
+            return {
+                // known field
+                text: `${window.digits(mindunits)} Mus (~${calcMUStr})`,
+                mindunits: mindunits
+            }
+        } else {
+            return {
+                // only calculated
+                text: `~${calcMUStr} Mus`,
+                mindunits: calcMU.mindunits
+            }
+        }
+
+    }
+
+    private async getDTPolygonText(polygon: L.Polygon): Promise<{ text: string, mindunits: number }> {
+
+        // TODO: add S2 Polygon Region
+
+        let total = 0;
+        let missing = false;
+
+        const ll = polygon.getLatLngs();
+        for (let i = 2; i < ll.length; i++) {
+            const latLngs = [ll[0], ll[i - 1], ll[i]];
+
+            const calcMU = this.muDB.calcMU(latLngs);
+            total += calcMU.mindunits;
+            missing ||= calcMU.missing;
+        }
+
+        const calcMUStr = (missing ? " >" : "") + window.digits(total);
+
+        return {
+            // only calculated
+            text: `~${calcMUStr} Mus`,
+            mindunits: total
+        }
+
     }
 
 
@@ -248,6 +302,57 @@ class LogFields implements Plugin.Class {
     }
 }
 
+
+
+
+
+    toggleTracking(): void {
+        if (this.trackingActive) this.disableTracking();
+        else this.enableTracking();
+    }
+
+    disableTracking() {
+        this.trackingActive = false;
+        this.hideTooltip();
+        window.map.off("mousemove", this.onMouseMove)
+        $("#logfieldbutton").removeClass("active");
+    }
+
+    enableTracking() {
+        this.trackingActive = true;
+        window.map.on("mousemove", this.onMouseMove);
+        $("#logfieldbutton").addClass("active");
+    }
+
+    onMouseMove = (ev: L.LeafletMouseEvent) => {
+        window.clearTimeout(this.mouseDelayTimer);
+        this.mouseDelayTimer = window.setTimeout(() => this.checkForTooltip(ev), TOOLTIP_DELAY);
+
+        this.hideTooltip();
+    }
+
+    showTooltip(latlng: L.LatLng, text: string): void {
+        if (!this.tooltip) {
+            this.tooltip = $("<div>", {class: "logfield-tooltip"});
+            const pane = window.map.getPanes()["popupPane"]; // FIXME <- does this work in leaflet 0.77???
+            this.tooltip.appendTo(pane);
+        }
+
+        this.tooltip.html(text);
+
+        const point = window.map.latLngToLayerPoint(latlng);
+        // this.tooltip.css( { left: point.x+12, top: point.y } )
+        // this.tooltip.offset({left: point.x+12, top: point.y});
+        L.DomUtil.setPosition(this.tooltip[0], point.add(L.point(12,0)));
+    }
+
+    hideTooltip(): void {
+        if (this.tooltip) {
+            this.tooltip.remove();
+            this.tooltip = undefined;
+            this.clearS2Cells();
+        }
+    }
 
 
 
