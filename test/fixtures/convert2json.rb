57d84b0b26fce0090cda8822240bc95ce355caab
McBen <viertelvor12@gmx.net>
2024-10-25 17:48:59 +0200

    test: more fields

--- test/fixtures/convert2json.rb
+++ test/fixtures/convert2json.rb
@@ -1,20 +1,24 @@
 require "csv"
 require "json"
 
-FILENAME = "fields_2024_10_20 123649.csv"
+allData = []
 
-data = CSV.read(FILENAME, headers: true)
+Dir.glob("fields*.csv").each do |fname|
+  data = CSV.read(fname, headers: true)
 
-toObject = data.map do |row|
-  { points: [
-    { lat: row[0].to_f, lng: row[1].to_f },
-    { lat: row[2].to_f, lng: row[3].to_f },
-    { lat: row[4].to_f, lng: row[5].to_f },
-  ],
-    mu: row[6].to_i }
+  toObject = data.map do |row|
+    { points: [
+      { lat: row[0].to_f, lng: row[1].to_f },
+      { lat: row[2].to_f, lng: row[3].to_f },
+      { lat: row[4].to_f, lng: row[5].to_f },
+    ],
+      mu: row[6].to_i }
+  end
+
+  allData += toObject
 end
 
-split = toObject.shuffle.each_slice(toObject.length / 2)
+split = allData.shuffle.each_slice((allData.length / 2.0).ceil)
 
 split.each_with_index do |selection, index|
   File.open("fields_#{index + 1}.json", "w") do |f|
